0e0c00ac2d4f97b0e32e196afeae97540036a642
Kenn <uccskf@gmail.com>
2019-09-20 10:59:18 +0530

    First Commit

---
+++ MyDemoForm1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MyDemoForm1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Stalin\source\repos\MyDemoForm1\MyDemoForm1\MyEXDatabase.mdf;Integrated Security = True
+        //Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename="\MyEXDatabase.mdf";Integrated Security = True
+        //Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename="|DataDirectory|\MyEXDatabase.mdf";Integrated Security = True
+        //Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"|DataDirectory|\\MyEXDatabase.mdf\";Integrated Security = True
+
+        SqlConnection con = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"|DataDirectory|\\MyEXDatabase.mdf\";Integrated Security = True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+        
+        private String getUserNameData()
+        {
+            con.Open();
+            String syntax = "SELECT Val FROM AdminTable WHERE MyID = 'UserName'";
+            cmd = new SqlCommand(syntax, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            String name = dr[0].ToString();
+            con.Close();
+            //MessageBox.Show(dr[0].ToString());
+            return name;
+        }
+        private String getPasswordData()
+        {
+            con.Open();
+            String syntax = "SELECT Val FROM AdminTable WHERE MyID = 'Password'";
+            cmd = new SqlCommand(syntax, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            String name = dr[0].ToString();
+            con.Close();
+            //MessageBox.Show(dr[0].ToString());
+            return name;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String UName = getUserNameData();
+            String PWD = getPasswordData();
+            String name, password;
+            name = textBox1.Text;
+            password = textBox2.Text;
+
+            if(name.Equals(UName) && password.Equals(PWD))
+            {
+                //Login Success
+                label3.Hide();
+                //MessageBox.Show("Login Success");
+
+                MainForm MF = new MainForm();
+                this.Hide();
+                MF.Show();
+            }
+            else{
+                //Login Failure
+                //MessageBox.Show("Login Failed");
+                label3.Show();
+
+            }
+        }
+    }
+}
---
+++ MyDemoForm1/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MyDemoForm1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            ShowItemsData();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"|DataDirectory|\\MyEXDatabase.mdf\";Integrated Security = True");
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String ItemName = textBox1.Text;
+            int ItemPrice = Int32.Parse(textBox2.Text);
+            String ItemType = textBox4.Text;
+
+            SqlCommand cmd = new SqlCommand("InsertItem", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@name", ItemName);
+            cmd.Parameters.AddWithValue("@price", ItemPrice);
+            cmd.Parameters.AddWithValue("@type", ItemType);
+
+
+            con.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            con.Close();
+
+            MessageBox.Show("Inserted Item");
+
+            ShowItemsData();
+        }
+        private void ShowItemsData()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("ShowAllItems", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                DataSet dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+
+                con.Open();
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+                con.Close();
+
+                dataGridView1.DataSource = dataSet.Tables[0];
+
+                this.dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                this.dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                this.dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                this.dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
